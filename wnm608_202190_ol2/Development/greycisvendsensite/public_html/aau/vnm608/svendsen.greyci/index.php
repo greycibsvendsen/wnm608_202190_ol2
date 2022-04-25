4b22915dea3e27952b81ea4f75eeaa33e7fc40f0
Greyci Briggett Svendsen <100736093+greycibsvendsen@users.noreply.github.com>
2022-04-24 23:28:39 -0700

    Add files via upload

--- wnm608_202190_ol2/Development/greycisvendsensite/public_html/aau/vnm608/svendsen.greyci/index.php
+++ wnm608_202190_ol2/Development/greycisvendsensite/public_html/aau/vnm608/svendsen.greyci/index.php
@@ -19,7 +19,7 @@
                 <h1>Welcome to Doggyshop</h1> 
                 <h2>Welcome to an easier everyday life as a dog owner</h>
                    <p>We have a wide range so that your dog can get a feed mixture that suits his needs</p>
-                <a href="https://cdn.shopify.com/s/files/1/2721/1236/files/1._Header_original_Sundt_og_smagfuldt_hundemad.jpg?v=1643204405&width=720&height=&crop=center" class="btn">Try us now</a>
+                 <a href="https://cdn.shopify.com/s/files/1/2721/1236/files/1._Header_original_Sundt_og_smagfuldt_hundemad.jpg?v=1643204405&width=720&height=&crop=center" class="btn">Try us now</a>
             </div>
         </section>
 
@@ -31,6 +31,23 @@
     <div class="container" id=view window>
     <div class="view-window" style="background-image: url('https://www.nomnomnow.com/images/home/hero_pork_ingredients_big.jpg');"></div>
     </div>
+
+     <div class="container" id="intro">
+        <div class="card soft">
+        <h2>New member?</h2>
+        <p>If you have not yet become a part of Doggyshop, then you can sign your dog up today.</p>
+        
+        <p>Start with a free trial package.</p> 
+       <h3>Here are 5 benefits:</h3>
+
+        <ol>
+        <li>Super premium quality dog food</li>
+        <li>A tailor-made feeding plan for your dog</li>
+        <li>Delivery right to your door</li>
+        <li>Flexible membership without binding</li>
+        <li>Access to advice from feed and nutrition experts</li>
+        </ol>
+      </div>
     
 
 
